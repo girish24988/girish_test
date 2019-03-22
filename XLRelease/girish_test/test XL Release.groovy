79ccf7aa7bed7402a2e58ecf006dd90527fc810a
girish24988 <48819506+girish24988@users.noreply.github.com>
2019-03-22 12:03:21 +0530

    first version

---
+++ XLRelease/girish_test/test XL Release.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://g6-PC:5516/#/templates/Folder5dbf6412aab34f698ce93e702ea6c085-Release6a684dd818ef41d18a65b7a004bf5214/releasefile
+// XL Release version:   8.5.4
+// Date created:         Fri Mar 22 12:03:20 IST 2019
+
+xlr {
+  template('test XL Release') {
+    folder('girish_test')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-03-22T09:00:00+0530')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
